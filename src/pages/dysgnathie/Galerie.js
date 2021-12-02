2eea8eb2d8078cb3699bd779699b15baa34de1c5
Shao Cherng Kong <skong@ukaachen.de>
2021-12-02 21:28:29 +0800

    Add buttons for videos and pictures

--- src/pages/dysgnathie/Galerie.js
+++ src/pages/dysgnathie/Galerie.js
@@ -1,11 +1,51 @@
 import React, { useState } from 'react';
 
 const Galerie = () => {
+  const [mediaType, setMediaType] = useState('videos');
+
+  const changeMediaType = (event) => {
+    setMediaType(event.target.value);
+  };
+
+  const openModal = (event) => {
+    console.log(event.target.value);
+  };
+
+  const videoTitles = ['Bilaterale sagittale split Osteotomie', 'Unterkieferrückverlagerung'];
+
+  const pictureTitles = ['Distalbisslage', 'Mesialbisslage'];
 
   return (
     <div>
-      <button className="btn bg-green-700 text-white mr-2">Videos</button>
-      <button className="btn bg-green-700 text-white mr-2">Bilder</button>
+      <div className="mb-4">
+        <button className="btn bg-green-500 hover:bg-green-700 text-white mr-2" onClick={changeMediaType} value="videos">Videos</button>
+        <button className="btn bg-green-500 hover:bg-green-700 text-white mr-2" onClick={changeMediaType} value="bilder">Bilder</button>
+      </div>
+
+      
+      <div className="flex gap-5">
+        {mediaType === 'videos' && 
+          <>
+            {videoTitles.map((videoTitle, index) =>
+              <>
+                <button key={index} onClick={openModal} className="rounded-md px-2 py-1 bg-green-200 hover:bg-green-400 text-gray-800" value={videoTitle.toLowerCase().split(' ').join('')}>{videoTitle}</button>
+                <img className="galleryPoster" src="thumbs/Play_experimentell.png" alt="" />
+              </>)
+            }
+          </>
+        }
+
+        {mediaType === 'bilder' && 
+          <>
+            {pictureTitles.map((pictureTitle, index) =>
+              <>
+                <button key={index} onClick={openModal} className="rounded-md px-2 py-1 bg-green-200 hover:bg-green-400 text-gray-800" value={pictureTitle.toLowerCase().split(' ').join('')}>{pictureTitle}</button>
+                <img className="galleryPoster" src="thumbs/Play_experimentell.png" alt="" />
+              </>)
+            }
+          </>
+        }
+      </div>
     </div>
   );
 };
